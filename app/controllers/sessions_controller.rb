3814355f0c0ea9b131306cb9eb8690e3a835e93e
Fiorella Leon-Gomez <fioarlego1@msn.com>
2019-05-16 13:40:30 -0700

    Fix typos.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,10 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		user = User.find_by(email: params[:email])
-		if user && user.authenticate(params[:password])
-			sesssion[:user_id] = user.id
-			flass[:success] = 'Welcome back!'
+		@user = User.find_by(email: params[:email])
+		if @user && @user.authenticate(params[:password])
+			session[:user_id] = @user.id
+			flash[:success] = 'Welcome back!'
 			redirect_to '/posts'
 		else
 			flash[:warning] = "Invalid email or password. Please try again."
