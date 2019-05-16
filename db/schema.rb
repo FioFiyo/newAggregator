37c8398e3757e43be38905b59a6b8b95dd7aea94
Fiorella Leon-Gomez <fioarlego1@msn.com>
2019-05-16 12:33:18 -0700

    update schema with user entry

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_16_181008) do
+ActiveRecord::Schema.define(version: 2019_05_16_191245) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
