48e454cd5875f91cbd2c509eb8a332a8454b8b26
Fiorella Leon-Gomez <fioarlego1@msn.com>
2019-05-16 12:53:54 -0700

    Create sessions when logged in. Add error logic.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,22 @@
 class SessionsController < ApplicationController
+
+	def new
+	end
+
+	def create
+		user = User.find_by(email: params[:email])
+		if user && user.authenticate(params[:password])
+			sesssion[:user_id] = user.id
+			flass[:success] = 'Welcome back!'
+			redirect_to '/posts'
+		else
+			flash[:warning] = "Invalid email or password. Please try again."
+			redirect_to '/login'
+	end
+
+	def destroy
+		session[:user_id] = nil
+		flash[:success] = "Logged out! Hope to see you soon."
+		redirect_to welcome_index_path
+	end
 end
