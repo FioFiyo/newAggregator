20dd1ed23399f10d54af47cc23f1c16060ca612c
Fiorella Leon-Gomez <fioarlego1@msn.com>
2019-05-16 16:53:16 -0700

    Establish posts relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
 	has_secure_password
 
+	has_many :posts
+	accepts_nested_attributes_for :posts, allow_destroy: true
 	validates :email, presence: true, uniqueness: true
 end
