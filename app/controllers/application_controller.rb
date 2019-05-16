afd96365d8f5031d1b257ac42f53550917b6dc8f
Fiorella Leon-Gomez <fioarlego1@msn.com>
2019-05-16 14:49:39 -0700

    Add helper showing logged in as

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,12 @@ class ApplicationController < ActionController::Base
 		@current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
 	end
 
+	def session_info
+		@user_email ||= current_user[:email] ? current_user[:email] : ''
+	end
+
 	helper_method :current_user
+	helper_method :session_info
 
 	def authenticate_user!
 		redirect_to '/login' unless current_user
