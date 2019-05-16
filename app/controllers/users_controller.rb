d616aa7b725497cdcba330385effb13e46bf1715
Fiorella Leon-Gomez <fioarlego1@msn.com>
2019-05-16 12:32:22 -0700

    Generate User model and controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		user = User.new(user_params)
+
+		if user.save
+			session[:user_id] = user.user_id
+			flash[:success] = "Welcome! You can start adding your news now."
+			redirect_to '/posts'
+		else
+			flash[:warning] = "Invalid email or password. Try again"
+			render :new
+		end
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:email, :password, :password_confirmation)
+	end
+end
