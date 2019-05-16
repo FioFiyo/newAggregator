bf85b0789300369337faae46358125bbc4ee7008
Fiorella Leon-Gomez <fioarlego1@msn.com>
2019-05-16 12:10:37 -0700

    Add comment TODO

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,7 @@ class PostsController < ApplicationController
 	end
 
 	def edit
+		#TODO
 		@post = Post.find(params[:id])
 	end
 
@@ -26,6 +27,7 @@ class PostsController < ApplicationController
 	end
 
 	def destroy
+		#TODO
 	end
 
 	private
