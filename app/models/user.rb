213243d7f0a74966478ab57317974088e631c958
Fiorella Leon-Gomez <fioarlego1@msn.com>
2019-05-16 17:42:11 -0700

    Comment model and associations to user and post added.Migration done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
 	has_secure_password
 
 	has_many :posts
+	has_many :comments
+	
 	accepts_nested_attributes_for :posts, allow_destroy: true
 	validates :email, presence: true, uniqueness: true
 end
