729da339c9447616f96734c9f16dc8363f530e4f
Fiorella Leon-Gomez <fioarlego1@msn.com>
2019-05-16 18:53:39 -0700

    Add comment relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
 
 	has_many :posts
 	has_many :comments
-	
+
 	accepts_nested_attributes_for :posts, allow_destroy: true
+	accepts_nested_attributes_for :comments, allow_destroy: true
+
 	validates :email, presence: true, uniqueness: true
 end
